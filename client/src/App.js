8bfcf23a1d20957d49d967b620d6eb8b5aca5966
Tab2624 <tabodansky@gmail.com>
2023-11-10 15:52:44 -0500

    initial functionality for login/logout buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import "./App.css";
+import LoginButton from "./components/login";
+import LogoutButton from "./components/logout";
+import { useEffect } from "react";
+import { gapi } from "gapi-script";
+
+const clientId =
+  "392990046278-n7s7ihp71s35g0o2ps53nh2e4g09soab.apps.googleusercontent.com";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+  useEffect(()=>{
+    function start(){
+      gapi.client.init({
+        clientId: clientId,
+        scope: ""
+      })
+    }
+    gapi.load('client:auth2', start)
+  })
+
+  return <div>
+    <LoginButton/>
+    <LogoutButton/>
+  </div>
 }
 
 export default App;
