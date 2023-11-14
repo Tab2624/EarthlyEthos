30d2c320f1da9561e43b0d0beb1d4d644a4a3a1b
Kierra Call <142943885+Kierra-Call@users.noreply.github.com>
2023-11-14 14:20:18 -0500

    update

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const AboutUs = (props) => {
   return (
-    <div className='large-margin-top-2'>
-        <img className='flex-short' src="..."></img>
-
+    <div className='large-margin-top-2 d-flex'>
+        <img className='flex-short border' src="https://i.pinimg.com/736x/fd/d3/24/fdd324c8c1bc83420825b7018c34ef1c.jpg" alt="caring for the planet"/>
+        <div className='flex-short-2'>
+            <h1>About Us</h1>
+            <hr />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae at, alias eaque a corrupti numquam quibusdam nostrum reprehenderit, dicta exercitationem voluptatum necessitatibus dolorum perferendis eveniet nisi reiciendis quaerat quisquam. Minima.</p>
+        </div>
     </div>
   )
 }
