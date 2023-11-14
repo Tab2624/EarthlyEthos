965d8e66244dfd97f8dc252d7f78a307341f440a
Tab2624 <tabodansky@gmail.com>
2023-11-13 19:07:16 -0500

    finished most of login functionality within the navbar

--- client/src/components/ProfilePhoto.jsx
+++ client/src/components/ProfilePhoto.jsx
@@ -1,62 +1,86 @@
-import React, { useEffect, useRef } from "react";
-import { connect } from "react-redux";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import { jwtDecode } from "jwt-decode";
 import store from "../store"; // Import the store
-import { useSelector } from "react-redux";
-import { Provider } from "react-redux";
 
-
-
-// const ProfilePhoto = ({ user }) => {
 const ProfilePhoto = (props) => {
+  // State to manage the profile box visibility
+  const [isProfileBoxOpen, setIsProfileBoxOpen] = useState(false);
 
+  // Retrieve user information from Redux state
+  const user = useSelector((state) => state.user);
+
+  // Reference to the sign-in div in the DOM
+  const signInDivRef = useRef(null);
+
+  // State to track user login status
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  // Function to open the profile popup
   const openProfilePopup = () => {
-    console.log("open")
-  }
+    setIsProfileBoxOpen(true);
+  };
+
+  // Function to close the profile popup
   const closeProfilePopup = () => {
-    console.log("close")
-  }
-  const user = useSelector(state => state.user); // Get the user from the Redux store
+    setIsProfileBoxOpen(false);
+  };
 
-  // create a sign in button with google
+  /*global google*/
   useEffect(() => {
-    /* global google */
+    // Check for the existence of the sign-in div
+    if (!signInDivRef.current) return;
+
+    // Initialize Google sign-in
     google.accounts.id.initialize({
-      client_id: "392990046278-n7s7ihp71s35g0o2ps53nh2e4g09soab.apps.googleusercontent.com",
-      callback: handleCallbackResponse,
+      client_id:
+        "392990046278-n7s7ihp71s35g0o2ps53nh2e4g09soab.apps.googleusercontent.com",
+      callback: handleCallbackResponse, // Callback function after sign-in
     });
-    // render the login with google button
-    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+
+    // Render the Google sign-in button
+    google.accounts.id.renderButton(signInDivRef.current, {
       theme: "outline",
-      size: "large",
+      size: "medium",
     });
-    // pop-up prompt for google sign in
-    google.accounts.id.prompt()
-  }, []);
 
-  
-  
+    // Prompt the Google sign-in
+    google.accounts.id.prompt();
+  }, [loggedIn]);
 
-  // grab user data and hide sign in div after sign in completion
+  // Function to handle the callback response after successful sign-in
   function handleCallbackResponse(response) {
     console.log("Encoded JWT ID token: ", response.credential);
     let userObject = jwtDecode(response.credential);
-    store.dispatch({ type: "SET_USER", payload: userObject }); // Dispatch user to Redux store
-    document.getElementById("signInDiv").hidden = true;
+
+    // Dispatch the user information to the Redux store
+    store.dispatch({ type: "SET_USER", payload: userObject });
+
+    // Hide the sign-in div
+    if (signInDivRef.current) {
+      signInDivRef.current.hidden = true;
+    }
+
+    // Update the login status
+    setLoggedIn(!loggedIn);
   }
 
-  // function to handle sign out
+  // Function to handle user sign-out
   function handleSignOut(event) {
-    store.dispatch({ type: "USER_LOGOUT" }); // Dispatching action directly using the store
-    document.getElementById("signInDiv").hidden = false;
+    // Dispatch the user logout action
+    store.dispatch({ type: "USER_LOGOUT" });
+
+    // Show the sign-in div
+    if (signInDivRef.current) {
+      signInDivRef.current.hidden = false;
+    }
+
+    // Update the login status
+    setLoggedIn(!loggedIn);
   }
 
-
-  
-
+  // JSX to render the profile photo and associated functionalities
   return user && user.email_verified ? (
-    <>
     <div className="justify-between d-flex align-items-center">
       <p>Welcome {user.name}!</p>
       <img
@@ -65,33 +89,37 @@ const ProfilePhoto = (props) => {
         height="30"
         src={user.picture}
         alt="person-male"
-        onClick={openProfilePopup()}
-        />
+        onClick={openProfilePopup} // Clicking to open the profile popup
+      />
+      {isProfileBoxOpen && ( // Display the profile box when open
+        <div
+          id="profileBox"
+          className="fixed p-3 border rounded bg-light border-dark">
+          <div className="justify-between d-flex align-items-center">
+            <h1>{user.name}</h1>
+            <button onClick={closeProfilePopup}>❌</button>{" "}
+            {/* Close the profile box */}
+          </div>
+          <hr />
+          {/* Various profile options */}
+          <button className="btn btn-outline-success">View Cart</button>
+          <button className="btn btn-outline-success">Orders</button>
+          <button className="btn btn-outline-success">Reviews</button>
+          <hr />
+          <button className="btn btn-danger" onClick={handleSignOut}>
+            Logout
+          </button>
+        </div>
+      )}
     </div>
-    <div id="profileBox" className="fixed p-3 border rounded bg-light border-dark">
-      <div className="justify-between d-flex align-items-center">
-    <h1>{user.name}</h1>
-    <button onClick={()=>closeProfilePopup()} className="">❌</button>
-      </div>
-    <hr />
-    <button className="btn btn-outline-success">View Cart</button>
-    <button className="btn btn-outline-success">Orders</button>
-    <button className="btn btn-outline-success">Reviews</button>
-    <hr />
-    <button className="btn btn-danger" onClick={handleSignOut}>Logout</button>
-    </div>
-        </>
   ) : (
     <div>
-      <div id="signInDiv"  hidden={user !== null}>Login</div>
+      {/* Display the login div */}
+      <div ref={signInDivRef} hidden={user !== null}>
+        Login
+      </div>
     </div>
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    user: state.user // Assuming 'user' is the key for user data in your Redux store
-  };
-};
-
-export default connect(mapStateToProps)(ProfilePhoto);
+export default ProfilePhoto;
